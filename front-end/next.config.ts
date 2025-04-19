7e1b1e7820fa3c60c897e358dfbda3f44d6359d1
kasra <kasramajdy81@gmail.com>
2025-04-19 12:03:33 +0330

    fixed: fix project for deploy

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
-  }
+  },
+  output: "export",
 };
 
 export default nextConfig;
